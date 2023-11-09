fe76b71a9a115261478a551a2d94e56693d0995d
Nrhiday4ti <nur.hidayati3102@gmail.com>
2023-11-09 12:17:40 +0800

    Lab2

---
+++ app/src/main/java/com/example/lab2_hidayati/MainActivity.java
@@ -0,0 +1,26 @@
+package com.example.lab2_hidayati;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Button;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button openActivity;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        openActivity = (Button) findViewById(R.id.button);
+    }
+
+    public void openActivity2(View view){
+        Intent intent = new Intent(this, MainActivity2.class);
+        startActivity(intent);
+    }
+}
